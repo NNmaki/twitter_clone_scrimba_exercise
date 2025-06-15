a59383212729d70cc7e31e41cc9733ac82807f22
NNmaki <nikon.niittymaki@gmail.com>
2025-06-15 16:01:43 +0300

    commit

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
-/*
-Challenge:
-1. Put all of the data in its own file called
-   data.js, and export it back into index.js. 
-   Make any changes to index.html that are
-   necessary to make this work.
-2. Log out tweetsData.
-*/
 
-import { tweetsData } from "./data.js";
+import { tweetsData } from './data.js'
 
-console.log(tweetsData)
+const tweetInput = document.getElementById('tweet-input')
+const tweetBtn = document.getElementById('tweet-btn')
+
+tweetBtn.addEventListener('click', function(){
+    console.log(tweetInput.value)
+})
+
+
+
+
+function getFeedHtml() {
+    
+    let feedHtml = ''
+    for (let tweet of tweetsData) {
+        
+        
+        feedHtml += `
+<div class="tweet">
+    <div class="tweet-inner">
+        <img src="./images/${tweet.profilePic}" class="profile-pic">
+        <div>
+            <p class="handle">${tweet.handle}</p>
+            <p class="tweet-text">${tweet.tweetText}</p>
+            <div class="tweet-details">
+                <span class="tweet-detail">
+                    ${tweet.replies.length}
+                </span>
+                <span class="tweet-detail">
+                    ${tweet.likes}
+                </span>
+                <span class="tweet-detail">
+                    ${tweet.retweets}
+                </span>
+            </div>   
+        </div>            
+    </div>
+</div>        
+        `
+    }
+    return feedHtml
+}
+
+
+
+getFeedHtml()
